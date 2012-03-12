ded2e0f7a50fafc4c5233c0d33f92e7f82102392
artemave <artemave@gmail.com>
2012-03-12 22:18:27 +0000

    fix test breakages because of not specifying json format

--- spec/controllers/tils_controller_spec.rb
+++ spec/controllers/tils_controller_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe TilsController do
+  render_views
+
   it "creates til" do
     assert_difference('Til.count') do
-      post :create, til: { content: 'awesome til' }
+      post :create, til: { content: 'awesome til' }, format: :json
     end
+    assert_response :success
   end
 
   it "reports error" do
     assert_no_difference('Til.count') do
-      post :create, til: {}
+      post :create, til: {}, format: :json
     end
     response.status.should_not == 200
-    response.body.should include("content can't be blank")
+    response.body.should include("can't be blank")
   end
 end
