8c736fcd3566057a880de1033c2911e04079f78a
artemave <artemave@gmail.com>
2012-05-21 21:39:26 +0100

    clean up env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,20 +1,17 @@
 require 'spork'
  
 Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
+  ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../../config/environment", __FILE__)
   require 'cucumber/rails'
   require 'factory_girl/step_definitions'
+  require 'capybara/firebug'
   require_relative 'paths'
 
   World(KnowsAboutPaths)
 
   Capybara.default_selector = :css
   Capybara.default_driver = :selenium
-
-  Before do
-    visit root_path
-  end
 end
  
 Spork.each_run do
@@ -22,22 +19,4 @@ Spork.each_run do
   
   DatabaseCleaner.orm = 'mongoid'
   DatabaseCleaner.strategy = :truncation
-  
-  # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
-  # See the DatabaseCleaner documentation for details. Example:
-  #
-  #   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  #     DatabaseCleaner.strategy = :truncation, {:except => %w[widgets]}
-  #   end
-  #
-  #   Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
-  #     DatabaseCleaner.strategy = :transaction
-  #   end
-  #
-  
-  # Possible values are :truncation and :transaction
-  # The :transaction strategy is faster, but might give you threading problems.
-  # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-  Cucumber::Rails::Database.javascript_strategy = :truncation
-
 end
